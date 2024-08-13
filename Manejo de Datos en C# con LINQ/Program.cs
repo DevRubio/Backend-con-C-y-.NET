315b66774eea33a31e5764dcb05a392cefb6fb67
Oscar Rubio <ofrubio0@gmail.com>
2024-08-13 11:34:26 -0500

    (UPDATE) Se crea la coleccion librosCollection para traer todos los libros y se crea metodo ImprimirValores para mostrar la coleccion por pantalla

--- Manejo de Datos en C# con LINQ/Program.cs
+++ Manejo de Datos en C# con LINQ/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿LinqQueries queries = new LinqQueries();
+
+ImprimirValores(queries.TodaLaColecion());
+
+void ImprimirValores(IEnumerable<Book> listadelibros)
+{
+    Console.WriteLine("{0,-60} {1,15} {2,15}\n","Title","N. Paginas","Fecha publicacion");
+    foreach(var item in listadelibros )
+    {
+        Console.WriteLine("{0,-60} {1,15} {2,15}", item.Title, item.PageCount, item.PublishedDate.ToShortDateString());
+    }
+}

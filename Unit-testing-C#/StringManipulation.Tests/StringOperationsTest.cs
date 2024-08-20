be2c06867f34c4d94b4a73b0b4bc3ec18179eec5
Oscar Rubio <ofrubio0@gmail.com>
2024-08-20 10:57:08 -0500

    (ADD) Se crea prueba unitaria para ConcatenateStrings

---
+++ Unit-testing-C#/StringManipulation.Tests/StringOperationsTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace StringManipulation.Tests
+{
+    public class StringOperationsTest
+    {
+        [Fact]
+        public void ConcatenateStrings()
+        {
+            var strOperation = new StringOperations();
+            var result = strOperation.ConcatenateStrings("Hello", "World");
+            Assert.Equal("Hello World", result);
+        }
+    }
+}
